8ce8179e581ed52a7773e5a456b4f9d575813b0b
franferrer <fmgf17@outlook.com>
2015-04-20 01:38:15 +0100

    reactive communicative now performs better

--- AASMAPlayer/Agents/ReactiveCommunicativeAgents/ReactiveCommunicativeContainer.cs
+++ AASMAPlayer/Agents/ReactiveCommunicativeAgents/ReactiveCommunicativeContainer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using PH.Common;
 using System.Drawing;
+using AASMAHoshimi;
 
 namespace AASMAHoshimi.ReactiveCommunicativeAgents
 {
@@ -14,62 +15,100 @@ namespace AASMAHoshimi.ReactiveCommunicativeAgents
         private List<Point> aznPointToVisit = new List<Point>();
         private List<Point> needlesPoints = new List<Point>();
         private Point aznPoint = new Point();
-        private bool _moving = false;
-        private bool _onNeedle = false;
-     
-
 
         public override void DoActions()
         {
-            //Starts to move if stopped and an azn point to visit waiting
-            if (_moving == false && aznPointToVisit.Count > 0 && _onNeedle == false)
-            {
-                getAASMAFramework().logData(this, "new route");
-                aznPoint = aznPointToVisit[0];
-                aznPointToVisit.RemoveAt(0);
-                this.MoveTo(aznPoint);
-                _moving = true;
-            }
-            //Stops if reaches location of azn point
-            if ( aznPoint != null  && Location.Equals(aznPoint) && _moving == true && _onNeedle == false)
-            {
-                getAASMAFramework().logData(this, "stop at azn point");
-                this.StopMoving();
-                _moving = false;     
-            }
+            List<Point> aznPoints;
+            List<Point> emptyNeedlePoints;
+            List<Point> fullNeedlePoints;
 
             //stock is the ammount of azn the collector already has. If full, there is no point in collecting more azn.
             //the overAZN method checks if the received nanobot is over an AZN point
-            if (Stock < ContainerCapacity && this.getAASMAFramework().overAZN(this) && _moving == false && _onNeedle == false)
+            if (this.State == NanoBotState.WaitingOrders && Stock < ContainerCapacity && this.getAASMAFramework().overAZN(this))
             {
-                //Debbug
                 getAASMAFramework().logData(this, "collecting azn...");
                 this.collectAZN();
             }
 
-            //if full and has needle to tranfer, goes there
-            if (Stock == 50 && needlesPoints.Count > 0 && _onNeedle == false )
-            {
-                getAASMAFramework().logData(this, "I'm full!! Going to needle");
-                this.MoveTo(needlesPoints[0]);
-                _moving = true;
-                _onNeedle = true;
-            }
-
             //transfer azn to needle
-            if ( _onNeedle == true && Location.Equals(needlesPoints[0]))
+            if (this.State == NanoBotState.WaitingOrders && getAASMAFramework().overEmptyNeedle(this) && Stock > 0)
             {
-                this.transferAZN();
-                _moving = false;
+                getAASMAFramework().logData(this, "transfer azn to needle");
+                if (Location.Equals(needlesPoints[0]))
+                {
+                    this.transferAZN();
+                }
             }
 
-            //ends needle transfer and sets container to new azn collection
-            if (_onNeedle == true && Stock == 0 && _moving == false)
+            //goes to an azn point to visit waiting
+            if (Stock == 0 && aznPointToVisit.Count > 0)
             {
-                needlesPoints.RemoveAt(0);
-                _onNeedle = false;
+                getAASMAFramework().logData(this, "new route");
+                aznPoint = aznPointToVisit[0];
+                //aznPointToVisit.RemoveAt(0);
+                this.MoveTo(aznPoint);
             }
-           
+
+            //has needle to tranfer, goes there
+            if (Stock == ContainerCapacity && this.State == NanoBotState.WaitingOrders && Stock > 0 && aznPointToVisit.Count > 0)
+            {
+                if (needlesPoints.Count > 0)
+                {
+                    getAASMAFramework().logData(this, "I'm full!! Going to needle");
+                    this.MoveTo(needlesPoints[0]);
+                }
+            }
+
+            if (this.State == NanoBotState.WaitingOrders && Stock > 0 && aznPointToVisit.Count > 0)
+            {
+                aznPoints = this.getAASMAFramework().visibleAznPoints(this);
+                emptyNeedlePoints = this.getAASMAFramework().visibleEmptyNeedles(this);
+                fullNeedlePoints = this.getAASMAFramework().visibleFullNeedles(this);
+
+                foreach (Point p in aznPoints)
+                {
+                    if (!aznPointToVisit.Contains(p))
+                    {
+                        aznPointToVisit.Add(p);
+                    }
+                }
+
+                foreach (Point empty in emptyNeedlePoints)
+                {
+                    if (!needlesPoints.Contains(empty))
+                    {
+                        needlesPoints.Add(empty);
+                    }
+                }
+
+                foreach (Point full in fullNeedlePoints)
+                {
+                    if (needlesPoints.Contains(full))
+                    {
+                        needlesPoints.Remove(full);
+                    }
+                }
+            }
+
+            if (this.State == NanoBotState.WaitingOrders)
+            {
+                getAASMAFramework().logData(this, "moving randomly");
+                if (frontClear())
+                {
+                    if (AASMAHoshimi.Utils.randomValue(100) < 90)
+                    {
+                        this.MoveForward();
+                    }
+                    else
+                    {
+                        this.RandomTurn();
+                    }
+                }
+                else
+                {
+                    this.RandomTurn();
+                }
+            }
         }
 
         public override void receiveMessage(AASMAMessage msg)
@@ -77,30 +116,14 @@ namespace AASMAHoshimi.ReactiveCommunicativeAgents
 
             try
             {
-                if (msg.Content.Equals("I've found an AZN POINT"))
+                if (msg.Content.Equals("I've visited a AZN POINT"))
                 {
-                    getAASMAFramework().logData(this, "somebody found a azn point");
+                    getAASMAFramework().logData(this, "somebody visited a azn point");
                     Point p = (Point)msg.Tag;
                     if (!aznPoint.Equals(p) && !aznPointToVisit.Contains(p))
                     {
                         aznPointToVisit.Add(p);
                     }
-                }
-
-                if (msg.Content.Equals("I'm in a AZN POINT"))
-                {
-                    getAASMAFramework().logData(this, "somebody is in a azn point");
-                    Point p = (Point)msg.Tag;
-                    if (aznPointToVisit.Contains(p))
-                    {
-                        aznPointToVisit.Remove(p);
-                    }
-                    if(aznPoint.Equals(p) && _moving == true)
-                    {
-                        _moving= false;
-                        this.StopMoving();
-                    }
-
                 }
 
                 if (msg.Content.Equals("Needle Position"))
@@ -110,18 +133,19 @@ namespace AASMAHoshimi.ReactiveCommunicativeAgents
                     if (!needlesPoints.Contains(p))
                     {
                         needlesPoints.Add(p);
-                    }     
+                    }
                 }
 
-                
-            }catch (Exception e)
+
+            }
+            catch (Exception e)
             {
-              //TODO
-              getAASMAFramework().logData(this.getAASMAFramework().NanoBots[0], "EXCEPTION IN AZN RECEIVE MESSAGE" + e.ToString());
+                //TODO
+                getAASMAFramework().logData(this.getAASMAFramework().NanoBots[0], "EXCEPTION IN AZN RECEIVE MESSAGE" + e.ToString());
             }
 
-          
 
-            }
-        }  
+
+        }
+    }
 }
